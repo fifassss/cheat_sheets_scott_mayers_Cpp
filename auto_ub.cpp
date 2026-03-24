aafd797ddc5f416a2c8d492044a6baa548c4fda2
fifassss <133858543+fifassss@users.noreply.github.com>
2026-03-24 14:46:45 +0700

    Create auto_ub.cpp
    неопределенное поведение auto

---
+++ auto_ub.cpp
@@ -0,0 +1,24 @@
+// AUTO и неопределенное поведение начало(вспомогательное)
+
+class sensor
+{
+public:
+    bool HighTemperatureDetected = true;
+    bool smokeDetected = true;
+};
+
+std::vector<bool> features(const sensor& s)
+{
+    return std::vector<bool> {s.HighTemperatureDetected,s.smokeDetected};
+}
+
+// AUTO и неопределенное поведение конец(вспомогательное)
+
+    // AUTO и неопределенное поведение начало
+    
+    sensor sen;
+
+    auto Issmokedetector = features(sen)[1]; // обращение к bit_refence.Прокси классу.А не к bool.Auto очень плохо с ними работает.Ну с прокси классами.
+    auto Issmokedetector = static_cast<bool>(features(sen)[1]); // тут кастуем явно к bool. И получаем нормальный результат. auto name = static_cast<type>(выражение)
+    std::cout << Issmokedetector << '\n';
+    //AUTO и неопределенное поведение конец
