a99cd287ee440ae31b8466bc7be87d71a7c9a260
fifassss <133858543+fifassss@users.noreply.github.com>
2026-03-27 06:36:03 +0700

    Сделал разные переменные
    Сделал разные переменные для более понятных шпаргалок

--- auto_ub.cpp
+++ auto_ub.cpp
@@ -16,9 +16,9 @@ std::vector<bool> features(const sensor& s)
 
     // AUTO и неопределенное поведение начало
     
-    sensor sen;
+    //sensor sen;
 
     auto Issmokedetector = features(sen)[1]; // обращение к bit_refence.Прокси классу.А не к bool.Auto очень плохо с ними работает.Ну с прокси классами.
-    auto Issmokedetector = static_cast<bool>(features(sen)[1]); // тут кастуем явно к bool. И получаем нормальный результат. auto name = static_cast<type>(выражение)
-    std::cout << Issmokedetector << '\n';
+    auto Issmokedetector_two = static_cast<bool>(features(sen)[1]); // тут кастуем явно к bool. И получаем нормальный результат. auto name = static_cast<type>(выражение)
+    //std::cout << Issmokedetector << '\n';
     //AUTO и неопределенное поведение конец
